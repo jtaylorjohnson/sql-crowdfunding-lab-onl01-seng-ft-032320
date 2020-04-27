581e90747cb29cf601319812c9c245fabe3bc803
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 17:10:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   INNER JOIN pledges
   ON users.id = pledges.user_id
   GROUP BY projects.category
-  WHERER projects.category = 'music'"
+  WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
